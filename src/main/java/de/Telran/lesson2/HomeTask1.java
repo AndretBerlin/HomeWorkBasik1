2dae2f1438d1a2f4ed631adc15f60696e0894d1b
Tel-ran.de <ne.calab2020@gmail.com>
2023-09-02 00:35:21 +0200

    ADD lesson 2

---
+++ src/main/java/de/Telran/lesson2/HomeTask1.java
@@ -0,0 +1,21 @@
+package de.Telran.lesson2;
+
+public class HomeTask1 {
+
+    public static void main(String[] args) {
+        char j ='G';
+        int l = 89;
+        byte i = 4;
+        short u = 56;
+        float p = 4.7333436f;
+        double g = 4.355453532;
+        long v = 12122;
+        System.out.println("char: " + j);
+        System.out.println("int: " + l);
+        System.out.println("byte: " + i);
+        System.out.println("short: " + u);
+        System.out.println("float: " + p);
+        System.out.println("double: " + g);
+        System.out.println("long: " + v);
+    }
+}
---
+++ src/main/java/de/Telran/lesson2/HomeTask2.java
@@ -0,0 +1,14 @@
+package de.Telran.lesson2;
+
+public class HomeTask2 {
+    public static void main(String[] args) {
+        int x = 789;
+        int y = x / 100;
+        int u = 4;
+        int i = 5;
+        char r = ',';
+        System.out.println("число: "+ x + " -> " + y + r + u + r + i);
+
+
+    }
+}
