6f3fc67a7fac1128a34ec9da5581d3b0c2328320
Tel-ran.de <ne.calab2020@gmail.com>
2023-09-03 11:54:26 +0200

    ADD lesson 2

--- src/main/java/de/Telran/lesson2/HomeTask2.java
+++ src/main/java/de/Telran/lesson2/HomeTask2.java
@@ -2,10 +2,10 @@ package de.Telran.lesson2;
 
 public class HomeTask2 {
     public static void main(String[] args) {
-        int x = 789;
+        int x = 345;
         int y = x / 100;
-        int u = 4;
-        int i = 5;
+        int u = x % 100 / 10;
+        int i = x % 10;
         char r = ',';
         System.out.println("число: "+ x + " -> " + y + r + u + r + i);
 
